aef3f7dba9d1fd4e3ee3475370c80aab60a2065c
Rreuben <jreuben07@gmail.com>
2018-05-04 18:09:52 +0300

    Complete business logic and user interface for ping-pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+//Business Logic
+
+function pingpong(startPoint) {
+  if ((startPoint % 3 === 0 && startPoint % 5 === 0)) {
+      return ('ping-pong');
+    } else if (startPoint %  5 === 0 ) {
+      return ('ping');
+    } else if (startPoint % 3 === 0) {
+      return ('pong');
+    } else {
+      return startPoint;
+    }
+}
+
+
+
+
+
+//User Interface
+
 $(document).ready(function() {
     $("#flip").click(function() {
         $("#panel").slideToggle("slow");
@@ -6,5 +26,15 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#flip1").click(function() {
         $("#panel1").slideToggle("slow");
+    });
+});
+$(document).ready(function() {
+    $('#submit').click(function(event) {
+    var userNumber = parseInt($('input#number').val());
+    event.preventDefault();
+        $('ul.numbers').empty();
+        for (var startPoint = 1; startPoint <= userNumber; startPoint += 1) {
+            $('ul.numbers').append('<ul>' + pingpong(startPoint) + '</ul>');
+            }
     });
 });
